3a125cb73ebbd04e1692c2c51c2a017c8b94f852
dongfangbeimu <95337470+dongfangbeimu@users.noreply.github.com>
2022-05-09 21:56:49 +0800

    Update jk.js

--- jk.js
+++ jk.js
@@ -3,14 +3,6 @@ IOS/安卓：
 
 最后一次修车了，目前适配小火箭自行挂上代理跑，0501最后一版
 
-重写：
-[task_local]
-#快手极速版
-[rewrite_local]
-[MITM]
-#IOS用第一个，安卓用第二个
-hostname = api.kuaisho*.com
-hostname = open.kuaisho*.com
  */
 const IM = f;
 (function (T, c) {
